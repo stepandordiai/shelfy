85b31de3a7750e1e008a8d21fab45dd648109e58
Stepan Dordiai <stepandordiai@gmail.com>
2025-08-02 14:18:15 +0200

    Update Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import { NavLink } from "react-router-dom";
 import heartIcon from "/heart.png";
 import heartIconRed from "/heart-red.png";
@@ -6,40 +7,43 @@ import "./Home.scss";
 const Home = ({ productsData, addToWish, wish }) => {
 	return (
 		<>
-			<div className="grid">
-				{productsData.map((el, index) => {
-					// TODO:
-
-					return (
-						<NavLink
-							key={index}
-							className="card-link"
-							to={`/product-page/${el.id}`}
-						>
-							<div className="cart-link__img-container">
-								<img src={el.img} alt="" />
-								<button
-									className="wish-btn"
-									onClick={(e) => {
-										// TODO:
-										e.preventDefault(); // Prevent NavLink navigation
-										e.stopPropagation(); // Stop bubbling up the click
-										addToWish(el);
-									}}
-								>
-									{wish.some((item) => item.id === el.id) ? (
-										<img src={heartIconRed} alt="" />
-									) : (
-										<img src={heartIcon} alt="" />
-									)}
-								</button>
-							</div>
-							<p style={{ marginTop: "10px" }}>{el.name}</p>
-							<p>$ {el.priceCents / 100}</p>
-						</NavLink>
-					);
-				})}
-			</div>
+			<Helmet>
+				<title>Shelfy - Official store</title>
+			</Helmet>
+			<main className="home">
+				<div className="grid">
+					{productsData.map((el, index) => {
+						return (
+							<NavLink
+								key={index}
+								className="card-link"
+								to={`/product-page/${el.id}`}
+							>
+								<div className="cart-link__img-container">
+									<img src={el.img} alt="" />
+									<button
+										className="wish-btn"
+										onClick={(e) => {
+											// TODO:
+											e.preventDefault(); // Prevent NavLink navigation
+											e.stopPropagation(); // Stop bubbling up the click
+											addToWish(el);
+										}}
+									>
+										{wish.some((item) => item.id === el.id) ? (
+											<img src={heartIconRed} width={20} height={20} alt="" />
+										) : (
+											<img src={heartIcon} width={20} height={20} alt="" />
+										)}
+									</button>
+								</div>
+								<p style={{ marginTop: "10px" }}>{el.name}</p>
+								<p>$ {el.priceCents / 100}</p>
+							</NavLink>
+						);
+					})}
+				</div>
+			</main>
 		</>
 	);
 };
